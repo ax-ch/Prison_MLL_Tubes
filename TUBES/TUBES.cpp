a518f45961f724496acd214aeeff854e52f28ebf
amalinamukhsis <mukhsisamalina@gmail.com>
2026-01-04 16:49:50 +0700

    Add functions to allocate Judge and Convict

--- TUBES/TUBES.cpp
+++ TUBES/TUBES.cpp
@@ -6,3 +6,31 @@ void createList(listJudge &L) {
     L.first = nullptr;
     L.last = nullptr;
 }
+
+adrJudge allocateJudge(string ID, string Name, string License, float AverageSentenceMonths) {
+    adrJudge J = new Judge;
+
+    J->info.ID = ID;
+    J->info.Name = Name;
+    J->info.License = License;
+    J->info.AverageSentenceMonths = AverageSentenceMonths;
+
+    J->prev = nullptr;
+    J->next = nullptr;
+    J->firstConvict = nullptr;
+
+    return J;
+}
+
+adrConvict allocateConvict(string ID, string Name, string Crime, int SentencedMonths) {
+    adrConvict C = new Convict;
+
+    C->info.ID = ID;
+    C->info.Name = Name;
+    C->info.Crime = Crime;
+    C->info.SentencedMonths = SentencedMonths;
+
+    C->next = nullptr;
+
+    return C;
+}
